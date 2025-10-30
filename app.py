0c94b842de7d5c2dbc76de7f89e0686324bcc2d4
alvarezmedinafrancisco <23308060610104@cetis61.edu.mx>
2025-10-30 10:56:16 -0500

    aa

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
 from flask import Flask , render_template, request , session
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/base')
 def inicio():
     return render_template('base.html')
 
 
 @app.route('/base',methods=["GET", "POST"])
 def calculadora():
-    if request.method == "GET":
-        grasas = request.form.get("grasas")
-        proteinas = request.form.get("proteinas")
-        carbohidratos = request.form.get("carbohidratos")
-        return render_template('base.html')
-    
-    
-@app.route("/clasificador", methods=["GET", "POST"])
-def clasificador():
-    if request.method =="POST":
-        grasas = request.form.get("grasas")
-        proteinas = request.form.get("proteinas")
-        carbohidratos = request.form.get("carbohidratos")
-        grasas == grasas*9
-        proteinas == proteinas*4
-        carbohidratos == carbohidratos*4
+    if request.method == "POST":
+        gras = request.form.get("grasas")
+        pro = request.form.get("proteinas")
+        car = request.form.get("carbohidratos")
         session['alimentos_clasificados']
+    return render_template("base.html" , prote = pro , carbo = car , gra = gras)
+
+@app.route("/clasificador", methods=["GET", "POST"])
+def nose():
     return render_template("clasificador.html")
 
 
